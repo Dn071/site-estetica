727deb9685d2e2aeb0bae4601b97b556a3884136
Daniel Oliveira <danieloliveirasilva1905@gmail.com>
2026-05-20 19:47:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const menuMobile = document.querySelector('.menu-mobile')
+
+const nav = document.querySelector('#nav')
+
+menuMobile.addEventListener('click', () => {
+
+    nav.classList.toggle('active')
+
+})
